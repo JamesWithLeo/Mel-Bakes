e525459f19ae57fb8bcb574a4d1fd190cf8d4790
James Leo <ocampojamesleo04@gmail.com>
2024-06-25 21:39:00 -0800

    tailwind

--- src/Product/ViewProduct.jsx
+++ src/Product/ViewProduct.jsx
@@ -20,7 +20,7 @@ function ViewProduct({
   };
   const flavors = ProductsObj[productIndex].flavors.map((flavor) => {
     return (
-      <h1 className="w-max rounded bg-white px-2 py-1 text-xs text-primary">
+      <h1 className="w-max rounded bg-gray-400 px-1 py-0 text-xs text-white sm:px-2 sm:py-1 sm:text-sm md:px-3">
         {flavor}
       </h1>
     );
@@ -28,30 +28,32 @@ function ViewProduct({
   return (
     <>
       <div
-        className="fixed z-10 flex h-svh w-full items-center justify-center bg-[#393664] opacity-90"
+        className="fixed z-10 flex h-svh w-full items-center justify-center bg-[#393664] opacity-70"
         id="ViewProduct__OutsideWrapper"
       ></div>
 
-      <PanelGroup className="fixed inset-x-0 bottom-0 z-10 mx-auto flex max-w-7xl flex-col justify-end bg-transparent sm:bottom-auto">
+      <PanelGroup className="fixed inset-x-0 bottom-0 z-10 mx-auto flex w-full flex-col bg-transparent sm:bottom-auto">
         <Panel
           className="h-full bg-transparent"
           onClick={exitViewProduct}
-          defaultSize={0}
+          defaultSize={25}
         ></Panel>
         <PanelResizeHandle className="h-4 w-full self-center rounded bg-gray-100" />
 
         <Panel
-          className="flex flex-col justify-between bg-white lg:justify-around"
-          defaultSize={100}
+          className="flex flex-col items-center justify-between bg-white lg:justify-around"
+          defaultSize={75}
         >
-          <button
-            className="m-1 w-max self-end text-gray-300"
-            onClick={exitViewProduct}
-          >
-            <FontAwesomeIcon icon={faXmark} id="exitViewProductIcon" />
-          </button>
+          <div className="flex w-full max-w-7xl flex-col">
+            <button
+              className="m-1 w-max self-end text-gray-300"
+              onClick={exitViewProduct}
+            >
+              <FontAwesomeIcon icon={faXmark} id="exitViewProductIcon" />
+            </button>
+          </div>
           <div
-            className="inset-x-0 z-10 mx-auto flex h-full max-w-7xl flex-col justify-center overflow-y-scroll bg-white lg:rounded"
+            className="inset-x-0 z-10 mx-auto flex h-full max-w-7xl flex-col justify-end overflow-y-scroll md:justify-center lg:rounded"
             id="ViewProduct__wrapper"
           >
             <div
@@ -62,12 +64,15 @@ function ViewProduct({
                 className="flex flex-col bg-transparent sm:flex-col md:w-1/2"
                 id="productProfile"
               >
+                <div className="ml-4 flex h-max w-max justify-center gap-4 rounded-md px-2 py-2 md:flex-col">
+                  {flavors}
+                </div>
                 <div
                   className="flex h-full w-full flex-col items-center"
                   id="cupcakeImgWrapper"
                 >
                   <img
-                    className="h-auto w-52 sm:w-64 md:w-80 lg:w-96"
+                    className="h-auto w-40 sm:w-64 md:w-80 lg:w-96"
                     src={ProductsObj[productIndex].image}
                     alt="cupcake"
                     id="productImgView"
@@ -79,32 +84,29 @@ function ViewProduct({
                 className="flex flex-col justify-center gap-1 md:w-1/2 md:gap-4"
                 id="cartNav"
               >
-                <div className="flex h-max w-max gap-4 rounded-md bg-gray-200 px-2 py-2">
-                  {flavors}
-                </div>
-                <div className="items-center rounded bg-white px-2">
+                <div className="flex flex-col items-center rounded bg-white px-2">
                   <h1
-                    className="md:text-1xl text-center font-[Raleway] text-xl lg:text-2xl"
+                    className="text-center font-[Raleway] text-xl font-medium text-primary md:text-2xl lg:text-3xl"
                     id="ProductName"
                   >
                     {ProductsObj[productIndex].name}
                   </h1>
                   <p
-                    className="items-center text-center font-[Raleway] text-sm text-[Goldenrod] md:text-base lg:text-xl"
+                    className="items-center text-center font-[Raleway] text-sm text-[Goldenrod] md:text-2xl"
                     id="ProductPrice"
                   >
                     {ProductsObj[productIndex].price}
                   </p>
                 </div>
-                <div className="w-full rounded bg-white px-2">
-                  <p className="text-xs">
+                <div className="flex w-full justify-center rounded bg-white px-2">
+                  <p className="text-justify text-xs">
                     Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                     Tenetur placeat iusto harum odit ipsam fugiat! Voluptatum
                     corrupti provident commodi. Vel, facilis officia? Mollitia
                     ratione in repellendus quisquam dolor vero veritatis?
                   </p>
                 </div>
-                <div className="flex w-max justify-center gap-4 px-2 align-middle">
+                <div className="flex w-full justify-center gap-4 px-2 align-middle">
                   <button>
                     <FontAwesomeIcon
                       id="hearIcon"
@@ -141,15 +143,15 @@ function ViewProduct({
                   </div>
                 </div>
 
-                <div className="flex flex-col gap-2 p-2 md:p-0">
+                <div className="flex flex-col items-center gap-2 p-2 md:p-0">
                   <button
-                    className="h-8 bg-[#f4eeff] text-xs text-primary sm:text-sm md:text-base"
+                    className="bg-secondarylight h-8 w-full text-xs text-primary sm:text-sm md:w-1/2 md:text-base"
                     id="addToCartButton"
                   >
                     Add to Cart
                   </button>
                   <button
-                    className="h-8 bg-primary text-xs text-white sm:text-sm md:text-base"
+                    className="h-8 w-full bg-primary text-xs text-white sm:text-sm md:w-1/2 md:text-base"
                     id="chechOutButton"
                   >
                     Buy Now
