cf4de6c2ec928994cde539972fd76dbd17609555
James Leo <136047998+JamesWithLeo@users.noreply.github.com>
2024-07-13 08:12:20 -0800

    Delete backend/routes directory

--- backend/routes/account.type.js
+++
@@ -1,8 +0,0 @@
-export const checkAccountType = async (req, res, next) => {
-  if (req.params.type === 'admin') {
-    res.send(`login/:type =  ${req.params.type}. Your are admin`);
-  }
-  else {
-    res.send('you are not admin');
-  }
-}
--- backend/routes/home.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const route = express.Router()
-
-export const home = route.get('/', async (req, res, next) => {
-  // this middleware called everytime routes changed
-  res.send(`Url: ${req.url}, baseUrl:${req.baseUrl} origUrl:${req.originalUrl}`);
-  next();
-})
