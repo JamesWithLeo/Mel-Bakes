edba4d1196ee302e4cd08e2fd3f4aa38ea75ef7b
James Leo <ocampojamesleo04@gmail.com>
2024-08-21 12:25:57 -0800

    error ui handling on server down

--- client/src/product.components/Product.tsx
+++ client/src/product.components/Product.tsx
@@ -5,31 +5,54 @@ import axios from "axios";
 import { IProduct } from "../AppDataTypes";
 import { useQuery } from "@tanstack/react-query";
 import { Navigate } from "react-router-dom";
+import { error } from "console";
+import { faFaceFrown } from "@fortawesome/free-regular-svg-icons";
 
 function Product() {
   const productQuery = useQuery({
     queryKey: ["product"],
     queryFn: async () => {
-      const response = await axios.get("melbake/cupcakes");
-      return await response.data;
+      return await axios.get("melbake/cupcakes").then((value) => {
+        return value.data;
+      });
     },
   });
 
-  if (productQuery.error) return <Navigate to={"/"} />;
   return (
     <>
-      {productQuery.fetchStatus !== "fetching" ? (
+      {!productQuery.isError ? (
         <>
-          <div
-            id="productContainer"
-            className="row-span-2 flex min-h-96 flex-row flex-wrap justify-center gap-4 sm:gap-5 md:gap-6"
-          >
-            {productQuery.data.length
-              ? productQuery.data.map((product: IProduct) => {
-                  return <ProductCard key={product._id} productObj={product} />;
-                })
-              : ""}
-          </div>
+          {productQuery.fetchStatus !== "fetching" ? (
+            <>
+              <div
+                id="productContainer"
+                className="row-span-2 flex min-h-96 flex-row flex-wrap justify-center gap-4 sm:gap-5 md:gap-6"
+              >
+                <>
+                  {productQuery.data && productQuery.data.length !== 0
+                    ? productQuery.data.map((product: IProduct) => {
+                        return (
+                          <ProductCard key={product._id} productObj={product} />
+                        );
+                      })
+                    : "hello"}
+                </>
+              </div>
+            </>
+          ) : (
+            <div
+              id="productContainer"
+              className="row-span-2 flex h-96 flex-col flex-wrap items-center justify-center gap-4 sm:gap-5 md:gap-6"
+            >
+              <FontAwesomeIcon
+                icon={faCircleNotch}
+                className="ml-2 animate-spin text-3xl text-secondarylight"
+              />
+              <h1 className="text-middle font-[Redhat] text-secondarylight">
+                Looking for baked cupcakes
+              </h1>
+            </div>
+          )}
         </>
       ) : (
         <div
@@ -37,11 +60,11 @@ function Product() {
           className="row-span-2 flex h-96 flex-col flex-wrap items-center justify-center gap-4 sm:gap-5 md:gap-6"
         >
           <FontAwesomeIcon
-            icon={faCircleNotch}
-            className="ml-2 animate-spin text-3xl text-secondarylight"
+            icon={faFaceFrown}
+            className="ml-2 text-3xl text-secondarylight"
           />
           <h1 className="text-middle font-[Redhat] text-secondarylight">
-            Looking for baked cupcakes
+            Cupcakes not found
           </h1>
         </div>
       )}
